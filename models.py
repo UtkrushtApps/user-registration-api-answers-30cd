304e002656546a50e377eed8d496fcde831eec58
Naman Muley <naman.g.muley@gmail.com>
2025-07-30 01:29:02 +0530

    Add models.py

---
+++ models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, UniqueConstraint
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    email = Column(String(255), unique=True, nullable=False, index=True)
+    password = Column(String(255), nullable=False)
+    __table_args__ = (
+        UniqueConstraint('email', name='uq_users_email'),
+    )
